b66e8826e75bb09d9bd243fcf6859cc9af72e605
ipconfig <ipconfig>
2017-05-08 17:28:57 +0530

    css auto scheduler

--- WebContent/js/app.js
+++ WebContent/js/app.js
@@ -2099,6 +2099,7 @@ function init_auto_scheduler()
 																			$('#admin_page_loader').hide();													
 																		});
 															});
+															$('.pagination .paginate_button').css('display','inline');
 													    }
 													});
 													$('#entity_course_holder').show();
@@ -2109,6 +2110,7 @@ function init_auto_scheduler()
 						
 						$(this).on( 'draw.dt', function () {
 						    callColumnHandlerFunctions();
+						    $('.pagination .paginate_button').css('display','inline');
 						});
 						$('.dataTables_info').hide();
 						
