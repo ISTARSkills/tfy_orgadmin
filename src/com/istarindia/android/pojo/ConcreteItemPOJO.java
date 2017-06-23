3bdcb69513899e8696fe62163949f62502879c0c
ipconfig <ipconfig>
2017-06-24 03:31:22 +0530

    ssss

--- src/com/istarindia/android/pojo/ConcreteItemPOJO.java
+++ src/com/istarindia/android/pojo/ConcreteItemPOJO.java
@@ -13,7 +13,7 @@ public class ConcreteItemPOJO implements Comparable<ConcreteItemPOJO>{
 	private String status;
 	private Integer orderId = 0;
 	private Integer taskId;
-	
+	private Integer progress;
 	
 	public ConcreteItemPOJO(){
 		
@@ -86,5 +86,14 @@ public class ConcreteItemPOJO implements Comparable<ConcreteItemPOJO>{
 	@Override
 	public int compareTo(ConcreteItemPOJO o) {
 		return this.orderId -o.orderId;
+	}
+	
+	@XmlAttribute(name = "progress", required = false)
+	public Integer getProgress() {
+		return progress;
+	}
+
+	public void setProgress(Integer progress) {
+		this.progress = progress;
 	}
 }
